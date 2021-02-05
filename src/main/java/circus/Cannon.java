2c4c8686c6495e821e322dcfefa3892b7f92e50d
Oscar Lai <e0424620@u.nus.edu>
2021-02-05 17:49:04 +0800

    no message

--- src/main/java/circus/Cannon.java
+++ src/main/java/circus/Cannon.java
@@ -5,7 +5,7 @@ public class Cannon extends Equipment {
     public Cannon(int purchasePrice) {
         super(purchasePrice);
     }
-
+//test
     @Override
     public int getValue() {
         return purchasePrice / 3;
